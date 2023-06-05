c48438fcf41340e856943187ad8e1bd56c81e5a5
ZygimantasL <129725364+ZygimantasL@users.noreply.github.com>
2023-06-05 10:38:09 +0300

    interval

--- js/23-interval.js
+++ js/23-interval.js
@@ -13,3 +13,12 @@ setTimeout(()=>{
 console.log('End');
 
 // pirma procesai kurie sinchroniski o po to isvykdo timeout interval funkcijas
+
+let i= 0;
+const timer = setInterval(() =>{
+    console.log('KA tu?...', i++);
+    if (i >= 100){
+        clearInterval(timer);
+    }
+}, 0);
+
